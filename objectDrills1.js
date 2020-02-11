afbe4f9f8266e80d5b60e47d9232cb05bca377fb
Hannah Thorpe <hthorpe@protonmail.com>
2020-02-11 14:53:16 -0500

    completed drills 1

---
+++ objectDrills1.js
@@ -0,0 +1,43 @@
+'use strict';
+function createMyObject(){
+  let object={};
+  return object={
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello: function(){
+      return 'hello';
+    }
+  };
+}
+function updateObject(obj){
+  obj.foo = 'foo';
+  obj.bar = 'bar';
+  obj.bizz = 'bizz';
+  obj.bang = 'bang';
+  return obj;
+}
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    // replace `null` with a function that uses self reference to return
+    // full name
+    fullName: function(){
+      return this.firstName +' '+ this.lastName;
+    },
+  };
+  return person;
+}
+const sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang',
+};
+  
+function keyDeleter(obj) {
+  delete obj.foo;
+  delete obj.bar;
+  return obj;
+}
